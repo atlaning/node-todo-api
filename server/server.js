1c4ccae64688b3a335d2d83992dccd26f32af3af
Andy <andy@evacmate.com>
2018-11-06 13:41:50 +1000

    Updated commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -24,6 +26,47 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// GET /todos/123123
+app.get('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// DELETE /todos/123123
+app.delete('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    user.save().then((userRes) => {
+        res.send(userRes);
     }, (e) => {
         res.status(400).send(e);
     });
